9fcd358fbe2eab4e5c9f50b113b5244c45f6eb02
KDG <airedale63@naver.com>
2023-02-15 16:05:43 +0900

    게시글 삭제 구현

--- src/com/koreaIT/java/BAM/Main.java
+++ src/com/koreaIT/java/BAM/Main.java
@@ -1,93 +1,111 @@
 package com.koreaIT.java.BAM;
-
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
-		System.out.println("==프로그램 시작==");
-
+		System.out.println("== 프로그램 시작 ==");
 		Scanner sc = new Scanner(System.in);
-
 		int lastArticleId = 0;
 		List<Article> articles = new ArrayList<>();
-
+		
 		while (true) {
-
+			System.out.printf("명령어) ");
 			String cmd = sc.nextLine().trim();
-
 			if (cmd.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
 				continue;
 			}
-
-			System.out.println("입력된 명령어 : " + cmd);
-
 			if (cmd.equals("exit")) {
-				System.out.println("==프로그램 끝==");
 				break;
 			} else if (cmd.equals("article write")) {
 				int id = lastArticleId + 1;
 				lastArticleId = id;
-				System.out.println("제목:");
-				String tittle = sc.nextLine();
-				System.out.println("내용:");
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-
-				Article article = new Article(id, tittle, body);
-
+				Article article = new Article(id, title, body);
+				
 				articles.add(article);
-
-				System.out.println(id + "번글이 생성되었습니다.");
-
+				System.out.printf("%d번 글이 생성되었습니다\n", id);
 			} else if (cmd.equals("article list")) {
+				
 				if (articles.size() == 0) {
 					System.out.println("게시글이 없습니다");
+					continue;
 				}
-					System.out.println("번호    | 제목");
-					for (int i = articles.size() - 1; i <= 0; i--) {
-						Article article = articles.get(i);
-						System.out.println(article.id + "    | " + article.tittle);
-					}
+				
+				System.out.println("번호	|	제목");
+				for (int i = articles.size() - 1; i >= 0; i--) {
+					Article article = articles.get(i);
+					System.out.printf("%d	|	%s\n", article.id, article.title);
+				}
+
 			} else if (cmd.startsWith("article detail ")) {
-				String[] cmdbits = cmd.split(" ");
-				int id = Integer.parseInt(cmdbits[2]);
+
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
 
 				Article foundArticle = null;
 
-				for (int i = 0; i < articles.size(); i++) {
+				for(int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
 
-					if (article.id == id) {
+					if(article.id == id) {
 						foundArticle = article;
-						continue;
+						break;
 					}
 				}
 
-				if (foundArticle == null) {
-					System.out.println(id + "번 게시글이 없습니다");
-			}
-				System.out.println("번호 : " + foundArticle.id);
-				System.out.println("제목 : " + foundArticle.tittle);
-				System.out.println("날짜 : " + foundArticle.id);
-				System.out.println("내용 : " + foundArticle.body);
-			}else if (cmd.startsWith("article delete")) {
+				if(foundArticle == null) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+
+				System.out.printf("번호 : %d\n", foundArticle.id);
+				System.out.printf("날짜 : %s\n", "2023-02-15 13:13:13");
+				System.out.printf("제목 : %s\n", foundArticle.title);
+				System.out.printf("내용 : %s\n", foundArticle.body);
+
+			}else if(cmd.startsWith("article delete")) {
+				String[] cmdBits = cmd.split(" ");
+				int id = Integer.parseInt(cmdBits[2]);
+
+				int foundIndex = 0;
+
+				for(int i = 0; i < articles.size(); i++) {
+					Article article = articles.get(i);
+
+					if(article.id == id) {
+						foundIndex = i;
+						break;
+					}
+				}
+				if(foundIndex == 0) {
+					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
+					continue;
+				}
+				System.out.println(articles.remove(foundIndex)); 
+				System.out.printf("%d번 게시글이 삭제되었습니다\n",id);
 				
 			}else {
 				System.out.println("존재하지 않는 명령어 입니다");
 			}
-			sc.close();
+		}
+		System.out.println("== 프로그램 끝 ==");
+		sc.close();
 	}
-	}
-class Article{
+}
+class Article {
 	int id;
-	String tittle;
+	String title;
 	String body;
 	
-	Article(int id, String tittle, String body){
-		this.id=id;
-		this.tittle=tittle;
-		this.body=body;
+	Article(int id, String title, String body){
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
